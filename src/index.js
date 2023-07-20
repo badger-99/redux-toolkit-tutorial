9cbc017b9a5590b04aa3e6ed65aac1b656744a0e
Alfred Peter <alpeter91@gmail.com>
2023-07-20 09:38:47 +0300

    Access navbar cart from store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { store } from './store';
+// redux imports
+import store from './store';
 import { Provider } from 'react-redux';
 
 const container = document.getElementById('root');
