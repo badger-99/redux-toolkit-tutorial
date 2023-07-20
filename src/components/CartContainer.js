d3d4a801b1d2d4d22391632fe32f7eb49c6e2e66
Alfred Peter <alpeter91@gmail.com>
2023-07-20 14:57:54 +0300

    Add modal functionality

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import CartItem from './CartItem';
 import { clearCart } from '../features/cart/cartSlice';
+import { openModal } from '../features/modal/modalSlice';
 
 const CartContainer = () => {
   const dispatch = useDispatch()
@@ -37,7 +38,7 @@ const CartContainer = () => {
             total <span>${total.toFixed(2)}</span>
           </h4>
         </div>
-        <button className='btn clear-btn' onClick={()=>{dispatch(clearCart())}}>clear cart</button>
+        <button className='btn clear-btn' onClick={()=>{dispatch(openModal())}}>clear cart</button>
       </footer>
     </section>
   );
--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cart/cartSlice';
+import modalReducer from './features/modal/modalSlice';
 
 // 'store' stores the states.
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    modal: modalReducer
   },
 });
 
