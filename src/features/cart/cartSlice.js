3caaceedbab35827862116695eba6dc0b8e89cbc
Alfred Peter <alpeter91@gmail.com>
2023-07-20 14:11:21 +0300

    Add calculate totals functionality

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,38 +1,49 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems";
+import { createSlice } from '@reduxjs/toolkit';
+import cartItems from '../../cartItems';
 
 const initialState = {
   cartItems: cartItems,
   amount: 4,
   total: 0,
   isLoading: true,
-}
+};
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
     clearCart: (store) => {
-      store.cartItems =[]
+      store.cartItems = [];
     },
     removeItem: (store, action) => {
-      const itemID = action.payload
+      const itemID = action.payload;
       store.cartItems = store.cartItems.filter((item) => item.id !== itemID);
     },
     toggleAmount: (store, { payload }) => {
-      const cartItem = store.cartItems.find((item) => item.id === payload.id)
+      const cartItem = store.cartItems.find((item) => item.id === payload.id);
       if (payload.operation === 'increase') {
-        cartItem.amount = cartItem.amount+1
+        cartItem.amount = cartItem.amount + 1;
       }
       if (payload.operation === 'decrease') {
-        cartItem.amount = cartItem.amount-1;
+        cartItem.amount = cartItem.amount - 1;
       }
-    }
-  }
+    },
+    calculateTotals: (state) => {
+      let quantity = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        quantity += item.amount;
+        total += item.price * item.amount;
+      });
+      state.amount = quantity;
+      state.total = total;
+    },
+  },
 });
 
 // console.log(cartSlice)
 
-export const { clearCart, removeItem, toggleAmount } = cartSlice.actions;
+export const { clearCart, removeItem, toggleAmount, calculateTotals } =
+  cartSlice.actions;
 
-export default cartSlice.reducer //if you log cartSlice, you'll notice  reducer is one of it's properties. we want to export the reducer specifically.
+export default cartSlice.reducer; //if you log cartSlice, you'll notice  reducer is one of it's properties. we want to export the reducer specifically.
