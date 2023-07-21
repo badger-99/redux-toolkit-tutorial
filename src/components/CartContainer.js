f5b5107af55577a867f2efe0169bc1a42d27830b
Alfred Peter <alpeter91@gmail.com>
2023-07-21 18:02:03 +0300

    Update page text

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -11,7 +11,7 @@ const CartContainer = () => {
       <section className='cart'>
         {/* cart header */}
         <header>
-          <h2>your bag</h2>
+          <h2>your cart</h2>
           <h4 className='empty-cart'>is currently empty</h4>
         </header>
       </section>
@@ -22,7 +22,7 @@ const CartContainer = () => {
     <section className='cart'>
       <header>
         {/* cart header */}
-        <h2>your bag</h2>
+        <h2>your cart</h2>
       </header>
       <div>
         {/* cart items */}
